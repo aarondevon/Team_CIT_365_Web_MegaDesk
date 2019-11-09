09be30b82d44a1e1b7fa99e5593986be7547224e
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-11-09 15:43:00 -0600

    start quote logic

--- CIT_365_Web_MegaDesk/Pages/Quotes/Create.cshtml.cs
+++ CIT_365_Web_MegaDesk/Pages/Quotes/Create.cshtml.cs
@@ -11,6 +11,7 @@ namespace CIT_365_Web_MegaDesk.Pages.Quotes
 {
     public class CreateModel : PageModel
     {
+
         private readonly CIT_365_Web_MegaDesk.Models.CIT_365_Web_MegaDeskContext _context;
 
         public CreateModel(CIT_365_Web_MegaDesk.Models.CIT_365_Web_MegaDeskContext context)
@@ -20,21 +21,66 @@ namespace CIT_365_Web_MegaDesk.Pages.Quotes
 
         public IActionResult OnGet()
         {
+            
+            SetQuoteDate = DateTime.Now;
             return Page();
         }
+        
+        [ViewData]
+        public DateTime SetQuoteDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string GetMaterial { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int GetRushDays { get; set; }
+
+        public int BasePrice { get; set; }
+
+        public int materialPrice { get; set; }
 
         [BindProperty]
         public Quote Quote { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public DateTime QuoteDate { get; set; }
 
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+            // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+            // more details see https://aka.ms/RazorPagesCRUD.
+            public async Task<IActionResult> OnPostAsync()
         {
+            BasePrice = 200;
+
+            switch (Quote.DeskMaterial)
+            {
+                case "Oak":
+                    materialPrice = 200;
+                    break;
+                case "Laminate":
+                    materialPrice = 100;
+                    break;
+                case "Pine":
+                    materialPrice = 50;
+                    break;
+                case "Rosewood":
+                    materialPrice = 300;
+                    break;
+                case "Veneer":
+                    materialPrice = 125;
+                    break;
+            }
+
+            Quote.QuoteDate = DateTime.Now;
+            Quote.DeskTopArea = Quote.Width * Quote.Depth;
+
+            Quote.QuoteTotal = BasePrice + materialPrice;
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
+
+         
             _context.Quote.Add(Quote);
             await _context.SaveChangesAsync();
 
